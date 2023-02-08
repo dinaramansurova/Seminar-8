63a558f02ac2c3b29b96339cf140c4a2cba0136c
Dinara <ash.solarium@gmail.com>
2023-02-08 19:52:23 +0300

    tasks

---
+++ Task53/Program.cs
@@ -0,0 +1,49 @@
+﻿// Task 53
+// Задайте двумерный массив. Напишите программу, которая поменяет местами
+// первую и последнюю строку массива.
+
+void FillMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(1, 11);
+}
+}
+}
+
+void ScreenMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+int temp = 0;
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+temp = matrix[0, j];
+matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+matrix[matrix.GetLength(0) - 1, j] = temp;
+}
+}
+
+
+Console.Clear();
+Console.Write("Ведите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+FillMatrix(matrix);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+ScreenMatrix(matrix);
---
+++ Task59/Program.cs
@@ -0,0 +1,67 @@
+﻿// Task 59
+// Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец,
+// на пересечении которых расположен наименьший элемент массива.
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+void ScreenMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int minIndexI = 0;
+    int minIndexJ = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                minIndexI = i;
+                minIndexJ = j;
+            }
+        }
+    }
+Console.WriteLine($"min = {min}; [{minIndexI}, {minIndexJ}]");
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            if (i != minIndexI)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j != minIndexJ)
+                        Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+}
+
+Console.Clear();
+int [] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+FillMatrix(matrix);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
